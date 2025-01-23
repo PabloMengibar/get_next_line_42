a2a02755d1646f551f1687adc0df428ba26a234a
Pablo Mengibar <pmengibar.dev@gmail.com>
2025-01-23 18:40:34 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,27 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmengiba <pmengiba@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 15:34:38 by pmengiba          #+#    #+#             */
-/*   Updated: 2025/01/14 15:34:42 by pmengiba         ###   ########.fr       */
+/*   Created: 2025/01/22 13:09:13 by pmengiba          #+#    #+#             */
+/*   Updated: 2025/01/23 14:24:14 by pmengiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1
 # endif
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <string.h>
 
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_strchr(char *str, int c);
+char	*ft_bulloc(int fd, char *s);
 char	*get_next_line(int fd);
-char    *ft_strdup(char *s);
-size_t    ft_strlen(char *s);
-char    *ft_substr(char *s, unsigned int start, size_t len);
-char    *ft_strjoin(char *s1, char *s2);
-void    fill_str(char *res, char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
+char	*ft_buffer(char *line);
+char	*ft_set_line(char *str);
 
 #endif
