e4eb759548c9658f41c348d7b4b53a4d0ac9736b
Pablo Mengibar <pmengibar.dev@gmail.com>
2025-01-23 18:40:19 +0100

    Update main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -5,33 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmengiba <pmengiba@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 13:55:19 by pmengiba          #+#    #+#             */
-/*   Updated: 2025/01/14 15:29:16 by pmengiba         ###   ########.fr       */
+/*   Created: 2025/01/23 13:07:44 by pmengiba          #+#    #+#             */
+/*   Updated: 2025/01/23 13:08:32 by pmengiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+int	main(int ac, char **av)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    fd = open("test.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error opening file");
-        return (1);
-    }
-    
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-
-    close(fd);
-    return (0);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+			return (1);
+		line = get_next_line(fd);
+		while (line)
+		{
+			printf("%s\n", line);
+			free(line);
+			line = get_next_line(fd);
+		}
+		free(line);
+		close(fd);
+	}
+	return (0);
 }
